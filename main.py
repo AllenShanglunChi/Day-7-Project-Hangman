6999ae8d494b5a9f7e800b0b23ee70bf20480d92
AllenShanglunChi <108212574+AllenShanglunChi@users.noreply.github.com>
2022-06-30 00:01:14 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+from hangman_words import word_list
+from hangman_art import logo, stages
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+#Or chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+end_of_game = False
+lives = 6
+
+# #Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks in display according to length of word
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+  
+while end_of_game == False:
+    guess = input("Guess a letter: ").lower()
+    #Check if letter already guessed right
+    if guess in display:
+      print(f"You have entered '{guess}', which you already guessed right.")
+
+    #Check guessed letter. Loop through each position in the chosen_word; If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+    for i in range(len(chosen_word)):
+        letter = chosen_word[i]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}") if want to see loop
+        if letter == guess:
+            display[i] = letter
+
+    #Check if user is wrong. End game if user loses all lives.
+    wrong_guess = []     
+    if (guess not in chosen_word) and (guess not in wrong_guess):         
+      print(f"You have entered '{guess}' which is not in the word. You lose one life.")
+      lives -= 1
+      wrong_guess += guess 
+     
+    #check if letter already guessed wrong
+    elif (guess not in chosen_word) and (guess in wrong_guess):
+      print(f"You have entered '{guess}', which you already guessed wrong.")        
+        
+    if lives == 0:
+      end_of_game = True
+      print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
