d1ea050b0545c639a52b06b047f6b65f40f9a3a3
AllenShanglunChi <108212574+AllenShanglunChi@users.noreply.github.com>
2022-07-02 15:58:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -20,38 +20,42 @@ for _ in range(len(chosen_word)):
 wrong_guess = []      
 while end_of_game == False:
     guess = input("Guess a letter: ").lower()
-    #Check if letter already guessed right
-    if guess in display:
-      print(f"You have entered '{guess}', which you already guessed right.")
-
-    #Check guessed letter. Loop through each position in the chosen_word; If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-    for i in range(len(chosen_word)):
-        letter = chosen_word[i]
-        # #see the loop
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}") 
-        if letter == guess:
-            display[i] = letter
-
-    #Check if user is wrong. End game if user loses all lives. Add wrong letters already guessed in wrong guess list.Check if already guessed wrong  
-    if (guess not in chosen_word) and (guess not in wrong_guess):         
-      print(f"You have entered '{guess}' which is not in the word. You lose one life.")
-      lives -= 1
-      if lives == 0:
-        end_of_game = True
-        print("You lose.")
-        
-      wrong_guess += guess 
-     
-    #for letters already guessed wrong
-    elif (guess not in chosen_word) and (guess in wrong_guess):
-      print(f"You have entered '{guess}', which you already guessed wrong.")        
-
-    #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
-
-    #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
-
-    print(stages[lives])
+    #Handle the case where the user accidentally enters more than one letter or anthing other than a letter.
+    if len(guess) >1 or str.isalpha(guess) == False:
+        print("please enter only one single letter of the alphabet at any one time!")
+    else:
+      #Check if letter already guessed right
+      if guess in display:
+        print(f"You have entered '{guess}', which you already guessed right.")
+  
+      #Check guessed letter. Loop through each position in the chosen_word; If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+      for i in range(len(chosen_word)):
+          letter = chosen_word[i]
+          # #see the loop
+          # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}") 
+          if letter == guess:
+              display[i] = letter
+  
+      #Check if user is wrong. End game if user loses all lives. Add wrong letters already guessed in wrong guess list.Check if already guessed wrong  
+      if (guess not in chosen_word) and (guess not in wrong_guess):         
+        print(f"You have entered '{guess}' which is not in the word. You lose one life.")
+        lives -= 1
+        if lives == 0:
+          end_of_game = True
+          print("You lose.")
+          
+        wrong_guess += guess 
+       
+      #for letters already guessed wrong
+      elif (guess not in chosen_word) and (guess in wrong_guess):
+        print(f"You have entered '{guess}', which you already guessed wrong.")        
+  
+      #Join all the elements in the list and turn it into a String.
+      print(f"{' '.join(display)}")
+  
+      #Check if user has got all letters.
+      if "_" not in display:
+          end_of_game = True
+          print("You win.")
+  
+      print(stages[lives])
